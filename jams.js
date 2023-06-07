2d7ec838cf88cde83d1de3178a42472c76e475da
jojo <116190164+jojodkks@users.noreply.github.com>
2023-06-07 14:57:30 -0300

    Create jams.js

---
+++ jams.js
@@ -0,0 +1,8 @@
+>console.group('copywrite');
+console.log('Copywriting é o processo de produção de textos persuasivos para ações de Marketing e Vendas, como o conteúdo de emails, sites, catálogos, anúncios e cartas de vendas. O profissional responsável pelo desenvolvimento do texto é conhecido como Copywriter.');
+
+ >console.group(' Designer Gráfico');
+ >console.group('Gestor de Tráfego');
+ >console.group(' Analista de Business  Inteligence ');
+
+console.groupEnd()
